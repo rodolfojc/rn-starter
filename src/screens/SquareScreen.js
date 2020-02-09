aa6ad791f2381fb8596fce456e31a61c8fc5d123
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-09 23:44:24 +0000

    Tying State Values Together

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import ColorCounter from '../Components/ColorCounter';
 
+const COLOR_INCREMENT = 15;
+
 const SquareScreen = (props) => {
 
     const [red, setRed] = useState(0);;
@@ -11,12 +13,21 @@ const SquareScreen = (props) => {
     return (
         <View>
             <ColorCounter 
-            onIncrease={() => setRed(red + 1)} 
-            onDecrease={() => setRed(red - 1)} 
+            onIncrease={() => setRed(red + COLOR_INCREMENT)} 
+            onDecrease={() => setRed(red - COLOR_INCREMENT)} 
             color="RED"
             />
-            <ColorCounter color="BLUE"/>
-            <ColorCounter color="GREEN"/>
+            <ColorCounter
+            onIncrease={() => setBlue(blue + COLOR_INCREMENT)} 
+            onDecrease={() => setBlue(blue - COLOR_INCREMENT)}  
+            color="BLUE"
+            />
+            <ColorCounter
+            onIncrease={() => setGreen(green + COLOR_INCREMENT)} 
+            onDecrease={() => setGreen(green - COLOR_INCREMENT)}   
+            color="GREEN"
+            />
+            <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red},${green},${blue})`}}/>
         </View>
     );
 };
