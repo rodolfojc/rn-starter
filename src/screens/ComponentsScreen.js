b6f33d3ea428a83ba851540fae546be80392dede
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-09 15:03:38 +0000

    One Common Error, Exercice

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -4,17 +4,23 @@ import { Text, StyleSheet, View} from 'react-native';
 const ComponentsScreen = () => {
 
     const greetting = 'Hola!!!';
-    const bye = <Text>Nos vemos</Text>
+    const bye = <Text>Nos vemos</Text>;
+    const name = 'Rodolfo';
 
-    return <View>
-                <Text style={styles.textStyle}>This is a component screen TESTED</Text>
-                <Text>{greetting}</Text>
-                {bye}
+    // Error for return statement when there is a block () or JSX element
+    return (
+            <View>
+                <Text style={styles.textStyle}>Getting started with React Native!</Text>
+                <Text style={styles.subHeader}>My name is {name} </Text>                
            </View>
+           )
 };
 
 const styles = StyleSheet.create({
     textStyle: {
+        fontSize: 45
+    },
+    subHeader: {
         fontSize: 20
     }
 });
