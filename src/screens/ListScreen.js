ab2ae91aae1baf472d994bb3bab44255dcf4f491
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-09 15:07:42 +0000

    Building Lists

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+
+const ListScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+};
+
+export default ListScreen;
