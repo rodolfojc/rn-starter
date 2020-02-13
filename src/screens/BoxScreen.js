2632ca3448ba7b9bac149c2d745df4d563f884da
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-13 17:20:17 +0000

    Exercise

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,44 +3,40 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
     return (
-        <View style={styles.viewStyle}>
-            <Text style={styles.textOneStyle}>Child #1</Text>
-            <Text style={styles.textTwoStyle}>Child #2</Text>
-            <Text style={styles.textThreeStyle}>Child #3</Text>
+        <View style={styles.parentStyle}>
+            <View style={styles.viewOneStyle}/>
+            <View style={styles.viewTwoStyle}/>
+            <View style={styles.viewThreeStyle}/>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    viewStyle: {
+    parentStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'                                    
+    },
+    viewOneStyle: {
+        borderWidth: 5,
+        borderColor: 'black',
+        backgroundColor: 'green',
+        height: 100,
+        width: 100,                   
+    },
+    viewTwoStyle: {
         borderWidth: 3,
         borderColor: 'black',
-        height: 200,
-        //alignItems,
-        //flexDirection,                   
+        backgroundColor: 'blue',
+        height: 100,
+        width: 100,
+        top: 100
     },
-    textOneStyle: {
+    viewThreeStyle: {
         borderWidth: 3,
-        borderColor: 'red',
-        //flex: 4                    
-    },
-    textTwoStyle: {
-        borderWidth: 3,
-        borderColor: 'red',
-        top: 10,
-        bottom: 0,
-        left: 0,
-        right:0,
-        position: 'absolute',
-        // ... StyleSheet.absoluteFillObject
-        //fontSize: 18
-        //alignSelf: 'center',
-        //flex: 4            
-    },
-    textThreeStyle: {
-        borderWidth: 3,
-        borderColor: 'red',
-        //flex: 2            
+        borderColor: 'black',
+        backgroundColor: 'red',
+        height: 100,
+        width: 100,          
     }
 });
 
