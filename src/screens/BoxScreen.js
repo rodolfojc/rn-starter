b4f8e9158a43dfe6fad8f5c30212b152c38c1131
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-13 09:44:11 +0000

    Layout with React Native - Box Object Model

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const BoxScreen = () => {
+    return (
+        <View>
+            <Text>Box Layout</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default BoxScreen;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -34,7 +34,11 @@ const HomeScreen = ({ navigation }) => {
             <Button 
               title="Go to Text Screen"
               onPress={() => navigation.navigate('Text')}
-            />                        
+            />
+            <Button 
+              title="Go to Box Screen"
+              onPress={() => navigation.navigate('Box')}
+            />                          
          </View>
          );
 };
